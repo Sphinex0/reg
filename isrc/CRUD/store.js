5049394c67654240afbe560bc3d9a8dedb46f3f3
ADNANE EL MIR <121230114+Sphinex0@users.noreply.github.com>
2024-02-09 22:17:30 +0100

    Delete isrc directory

--- isrc/CRUD/store.js
+++
@@ -1,5 +0,0 @@
-import { legacy_createStore } from "redux";
-import reducer from "./reduces/reducer";
-
-const store=legacy_createStore(reducer);
-export default store;
--- isrc/composants/liste.jsx
+++
@@ -1,8 +0,0 @@
-var elemens=["element1","element2","element3"];
-function Liste(){
-    return(<ul>{
-        elemens.map((e,i)=>{
-            return <li key={i}>{e}</li>
-        })}</ul>)
-}
-export default Liste;
